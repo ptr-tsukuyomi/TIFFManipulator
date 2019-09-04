c3d5a725c32d7cf1a2bba467d45cdeb1f188e3b4
tsukuyomi <tsukuyomi@outlook.jp>
2019-09-04 23:09:16 +0900

    add test project

---
+++ TIFFManipulatorTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TIFFManipulatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
